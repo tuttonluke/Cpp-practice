105244cda8320ec6dac1167b8576d1cf72e8db3c
Luke Tutton <tuttonluke@gmail.com>
2023-02-17 12:01:35 +0000

    Adds overview of data types.

--- variables_and_dtypes.cpp
+++ variables_and_dtypes.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main()
-{
+{   // Data Types
+    char grade = 'A'; // single quotation marks
+    string myString = "Girrafe"; // double quotes
+    int myInt = 27;
+    float myFloat = 2.8;
+    double myDouble = 2.3; // conventionally use doubles
+    bool myBool = true;
+
+    cout << grade << endl;
+    
+    // A story to demonstrate variable assignment
     string characterName = "John";
     int characterAge;
     characterAge = 35;
