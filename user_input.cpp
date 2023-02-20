58da1106c87b2162074270e1b3c3398fb7acf196
Luke Tutton <tuttonluke@gmail.com>
2023-02-20 17:41:32 +0000

    Adds a function requiring user input.

---
+++ user_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{   
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "You are " << age << " years old.";
+
+    return 0;
+}
