6bf9470f3f5cccefcd7e116125ae2c10b98c93fb
Luke Tutton <tuttonluke@gmail.com>
2023-02-20 17:13:29 +0000

    Adds some basic string operations.

--- strings.cpp
+++ strings.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "~" << endl;
-    cout << "Hello" << endl;
+    string phrase = "Panda Zoo";
+    cout << phrase.length() << endl; // calling length function
+    cout << phrase[0] << endl; // zero indexing
+    phrase[0] = 'B'; // strings are mutable in C++
+    cout << phrase << endl;
+    cout << phrase.find("Zoo", 0) << endl; // 2nd argument is starting index
+    
+    string phrasesub; // identify the new string
+    phrasesub = phrase.substr(3, 3); // starting index, substring length
+    cout << phrasesub;
+
     
     return 0;
 }
