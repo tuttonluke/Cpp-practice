13e0a83a9dabdd4f49d28c5c31c0f17b062d603b
Luke Tutton <tuttonluke@gmail.com>
2023-02-21 17:46:34 +0000

    Adds getline for inputing strings.

--- user_input.cpp
+++ user_input.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 int main()
 {   
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
+    // // Entering integers, doubles, or characters
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
 
-    cout << "You are " << age << " years old.";
+    // cout << "You are " << age << " years old." << endl;
+
+    // Entering strings
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello " << name << endl;
 
     return 0;
 }
