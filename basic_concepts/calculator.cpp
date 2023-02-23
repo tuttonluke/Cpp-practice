882d879fce90de50c38cd03efdcb8a19921031c9
Luke Tutton <tuttonluke@gmail.com>
2023-02-23 19:51:06 +0000

    Adds a better calculator with if statements.

--- basic_concepts/calculator.cpp
+++ basic_concepts/calculator.cpp
@@ -3,8 +3,19 @@
 
 using namespace std;
 
+// stubs
+int simpleCalc();
+double betterCalc();
+
 int main()
 {   
+    cout << betterCalc();
+
+    return 0;
+}
+
+
+int simpleCalc(){
     int num1, num2;
     cout << "Enter first number: ";
     cin >> num1;
@@ -12,7 +23,31 @@ int main()
     cout << "Enter second number: ";
     cin >> num2;
 
-    cout << num1 + num2;
+    return num1 + num2;
+}
 
-    return 0;
+double betterCalc(){
+    double num1;
+    double num2;
+    double result;
+    char op; // operator
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator: ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    if(op == '+'){
+        return num1 + num2;
+    } else if(op == '-') {
+        return num1 - num2;
+    } else if(op == '*') {
+        return num1 * num2;
+    } else if(op == '/') {
+        return num1 / num2;
+    } else {
+        cout << "Invalid operator.";
+    }
 }
