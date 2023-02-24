936bbacaf41f23ea8fdea8f9898a53a3f51134cd
Luke Tutton <tuttonluke@gmail.com>
2023-02-24 16:27:07 +0000

    Adds file for practicing for loops.

---
+++ basic_concepts/for_loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    
+    for(int i = 1; i <= 5; i++){
+        cout << i << endl;
+    }
+
+    // looping through an array
+    int nums[] = {1, 2, 5, 6, 3};
+    for(int i = 0; i < 5; i++){
+        cout << nums[i] << endl;
+    }
+
+    return 0;
+}
