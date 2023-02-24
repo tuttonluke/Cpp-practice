fc6fb7f9556e817d83397028fc9daa4da7889a50
Luke Tutton <tuttonluke@gmail.com>
2023-02-24 16:45:54 +0000

    Adds 2D array and nested for loop.

--- basic_concepts/arrays.cpp
+++ basic_concepts/arrays.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {   
+    // 1D arrays
     int luckyNums[20] = {4, 8, 5, 42};
 
     cout << luckyNums[0] << endl;
@@ -12,6 +13,21 @@ int main()
     luckyNums[0] = 19; //modify element
     cout << luckyNums[0] << endl;
     cout << luckyNums << endl;
-    
+
+    // 2D arrays
+    int numberGrid[3][2] = {
+                            {1, 2},
+                            {3, 4},
+                            {5, 6}
+                         };
+
+    // nested for loop
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 2; j++){
+            cout << numberGrid[i][j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
