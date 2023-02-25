88a95723132fbc634dd73c2f73f003f3582b7c4f
Luke Tutton <tuttonluke@gmail.com>
2023-02-25 13:34:54 +0000

    Adds examples of object functions and inheritance.

--- basic_concepts/classes_and_objects.cpp
+++ basic_concepts/classes_and_objects.cpp
@@ -21,6 +21,7 @@ class Book {
         }
 };
 
+// Constructors
 class Student {
     public:
         string name;
@@ -42,24 +43,96 @@ class Student {
         }
 };
 
+// Object functions
+class Movie {
+    private:
+        string rating;
+    public:
+        string title;
+        string director;
+
+
+        Movie(string aTitle, string aDirector, string aRating){
+            title = aTitle;
+            director = aDirector;
+            setRating(aRating);
+        }
+
+        void setRating(string aRating){
+            if(aRating == "U" || aRating == "PG"|| aRating == "12"|| aRating == "15"|| aRating == "18"|| aRating == "NR"){
+                rating = aRating;
+            } else {
+                rating = "NR";
+            }
+        }
+
+        string getRating(){
+            return rating;
+        }
+
+};
+
+// Inheritance
+class Chef {
+    public:
+        void makeChicken(){
+            cout << "The chef makes chicken." << endl;
+        }
+        void makeSalad(){
+            cout << "The chef makes salad." << endl;
+        }     
+        void makeSpecialDish(){
+            cout << "The chef makes bbq ribs." << endl;
+        }
+};
+
+class ItalianChef : public Chef {
+    public:
+        void makePasta() {
+            cout << "The chef makes pasta." << endl;
+        } 
+        void makeSpecialDish(){
+            cout << "The chef makes lasagne." << endl;
+        }
+};
+
+
+
 int main()
 {   
-
+    /* Simple class
     Book book1("Ensaio Sobre a Cegueira", "José Saramago", 200);
     Book book2("Cien Años de Soledad", "Gabriel García Márquez", 500);
     Book book3;
-
-    /*
     cout << book1.title << endl;
     cout << book2.author << endl;
     cout << book3.pages << endl;
     */
 
+
+    /* Constructors
     Student student1("James", "Buisness", 2.4);
     Student student2("Jane", "Physics", 3.6);
-
     cout << student1.hasHonours() << endl;
     cout << student2.hasHonours() << endl;
+    */
+
+
+    /* Object functions
+    Movie fellowship("Fellowship of the Ring", "Peter Jackson", "12");
+    cout << fellowship.getRating() << endl;
+    fellowship.setRating("Dog");
+    cout << fellowship.getRating() << endl;
+    */
+
+    // Inheritance
+    Chef chef;
+    ItalianChef italianChef;
+
+    italianChef.makeChicken();
+    italianChef.makePasta();
+    chef.makeSpecialDish();
+    italianChef.makeSpecialDish();
 
     return 0;
 }
